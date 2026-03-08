c429a07901c9a512f4f1f93653a842e4eef73c79
maverick0721 <shivang0721gupta@gmail.com>
2026-03-08 07:42:05 +0000

    Created text-to-speech in tts.py in voice dir

---
+++ voice/tts.py
@@ -0,0 +1,14 @@
+import pyttsx3
+
+
+class Speaker:
+
+    def __init__(self):
+
+        self.engine = pyttsx3.init()
+
+    def speak(self, text):
+
+        self.engine.say(text)
+
+        self.engine.runAndWait()
