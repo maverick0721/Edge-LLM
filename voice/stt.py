49cfb695919a752a5e6992a2e9bc9bf966c44be0
maverick0721 <shivang0721gupta@gmail.com>
2026-03-08 07:39:57 +0000

    Using an offline speech model in stt.py in voice dir

---
+++ voice/stt.py
@@ -0,0 +1,28 @@
+import json
+import sounddevice as sd
+from vosk import Model, KaldiRecognizer
+
+class SpeechRecognizer:
+
+    def __init__(self):
+
+        self.model = Model("vosk-model-small-en-us-0.15")
+
+    def listen(self):
+
+        rec = KaldiRecognizer(self.model, 16000)
+
+        with sd.RawInputStream(
+            samplerate=16000,
+            blocksize=8000,
+            dtype="int16",
+            channels=1
+        ) as stream:
+
+            data = stream.read(4000)[0]
+
+            if rec.AcceptWaveform(data):
+
+                result = json.loads(rec.Result())
+
+                return result["text"]
