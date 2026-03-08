4d159e24fd0c1f05bee1f8e7147c475681ce7234
maverick0721 <shivang0721gupta@gmail.com>
2026-03-08 07:36:30 +0000

    Integrating retrieval with the LLM in rag_pipeline.py in rag dir

---
+++ rag/rag_pipeline.py
@@ -0,0 +1,38 @@
+from rag.embedder import Embedder
+from rag.ingest import DocumentIngestor
+
+class RAGPipeline:
+
+    def __init__(self):
+
+        self.embedder = Embedder()
+
+        self.ingestor = DocumentIngestor()
+
+    def add_documents(self, docs):
+
+        self.ingestor.ingest(docs)
+
+    def retrieve(self, query):
+
+        store = self.ingestor.get_store()
+
+        q = self.embedder.embed([query])
+
+        return store.search(q)
+
+    def build_prompt(self, query):
+
+        docs = self.retrieve(query)
+
+        context = "\n".join(docs)
+
+        return f"""
+Use the following context to answer.
+
+Context:
+{context}
+
+Question:
+{query}
+"""
