36e3b830bbea2a6c0216ab4d1e787316606faefd
maverick0721 <shivang0721gupta@gmail.com>
2026-03-08 06:41:30 +0000

    Created Server in cli.py in cli dir

---
+++ cli/cli.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+def run_server():
+
+    uvicorn.run(
+        "server.websocket_server:app",
+        host="0.0.0.0",
+        port=8000,
+    )
+
+
+if __name__ == "__main__":
+    run_server()
